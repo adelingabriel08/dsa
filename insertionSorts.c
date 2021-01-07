6a6b9ed7049ca331bd93e2eb6e5e2d24f02aca0e
Adelin Gabriel Chiș <adelingabriel08@gmail.com>
2021-01-07 19:00:01 +0200

    fmm pseudocode

--- insertionSorts.c
+++ insertionSorts.c
@@ -1,18 +1,15 @@
 /*Sorting by straight insertion */
-type_element a[n];
-void SortingByInsertion()
- { 
- 	int i,j;
- 	type_element temp;
- 	for(i=2;i<n;i++)
- 	{
- 		temp= a[i];
- 		a[0]= a[i]; /*sentinel in position a[0]*/
- 		j= i-1;
- 		while(a[j].key>temp.key)
+void straight_insertion(int a[],int n)
+{
+	for(int i=n-2;i>=0;i--)
+	{
+ 		a[n]=a[i]; /*sentinel on position a[n]*/
+ 		int j=i+1;
+ 		while(a[j]<a[n])
  		{
-			 a[j+1]= a[j]; j= j-1; /*3.2.1.b*/
- 		}
- 		a[j+1]= temp; /*insertion*/
- 	 }
- }
+			 a[j-1]=a[j]; j++; /*3.2.1.e*/
+		 }
+ 			a[j-1]=a[n]; /*insertion*/
+	}
+}
+
